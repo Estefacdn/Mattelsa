bba74e667421fe6f0f134e5046b4ff6c62bb5edf
Estefania <estefacdn@gmail.com>
2021-10-19 19:25:49 -0500

    Base Tienda

---
+++ app/src/main/java/com/tienda/mattelsa/MainActivity.java
@@ -0,0 +1,38 @@
+package com.tienda.mattelsa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.tienda.mattelsa.databinding.ActivityMainBinding;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ActivityMainBinding mainBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mainBinding=ActivityMainBinding.inflate(getLayoutInflater());
+        View view = mainBinding.getRoot();
+        setContentView(view);
+
+        mainBinding.btnsignin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnsignin:
+                Intent intent= new Intent(this,RegisterActivity.class);
+                startActivity(intent);
+                break;
+        }
+
+    }
+}
